a794347c28510294f77e4af149f66b79f1cdbf64
Micro-Organism <17310708550@163.com>
2021-01-23 14:37:44 +0800

    commit initialize code

---
+++ Zero.AspNetAPIMain/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Zero.AspNetAPIMain.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Zero.AspNetAPIMain/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Zero.AspNetAPIMain.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
